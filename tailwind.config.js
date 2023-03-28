2cf590449c6bbf68a8270fff01f0de3690bd3367
Murhussain <husseinmurashi@yahoo.com>
2023-03-28 10:43:10 +0200

    Setting up color scheme for light theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,33 @@ module.exports = {
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Light color pallette
+        'primary': '#7586ec',
+        'accent': '#d6dbf9',
+        'secondary': '#ff7b1c',
+        'secondary-accent': '#ffe5d2',
+        'third': '#48D1D0',
+        'third-accent': '#daf6f6', 
+        'body': '#FFFFFF',
+        'body-accent': '#f9f9fb',
+        'black': '#0c0d18',
+        'black-accent': '#989898',
+        'gray': '#b8b9c1',
+
+        // Dark color pallette
+        'd-bg': '#0d1116',
+        'd-bg-accent': '#161C23',
+        'd-card': '#2d3339',
+        'd-body': '#8b8e91',
+        'd-accent-body': '#45494f',
+        'd-white': '#d1d1d1',
+        'd-accent-white': '#989898',
+      }
+    },
   },
   plugins: [],
 }
