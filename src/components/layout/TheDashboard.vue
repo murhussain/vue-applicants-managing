4ef9bd2c46d0d9fb51d19bd999d3f5e8fd777032
Murhussain <husseinmurashi@yahoo.com>
2023-03-28 14:31:31 +0200

    Adding penIcon to the project as a component

--- src/components/layout/TheDashboard.vue
+++ src/components/layout/TheDashboard.vue
@@ -9,16 +9,18 @@ import TheHeader from './TheHeader.vue';
     <TheSidebar />
 
     <!-- Main Content -->
-    <main class="flex-1 overflow-y-auto">
-      
+    <main class="flex-1 flex flex-col overflow-x-hidden overflow-y-auto">
+
       <!-- Header -->
       <TheHeader />
 
       <!-- Page Content -->
-      <div class="bg-green-300 px-4 py-8">
-        <!-- <slot></slot> -->
-        <p>Here comes main content</p>
-      </div>
+      <section className="p-4 bg-red-200 flex-1 overflow-x-hidden overflow-y-auto scrollbar-hide">
+        <div className="bg-green-100 min-h-full flex flex-col p-2 rounded-lg space-y-2">
+          <!-- <slot></slot> -->
+          <p>Here comes main content</p>
+        </div>
+      </section>
     </main>
   </div>
 </template>
--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -1,10 +1,14 @@
+<script setup>
+import IconPen from '../icons/IconPen.vue';
+</script>
+
 <template>
   <header class="bg-gray-200 h-16 flex items-center justify-between px-4">
-    <div>
-      Logo
-    </div>
-    <div>
-      Menu
+    <p>Senior java developer</p>
+    <div class="flex space-x-4">
+      <div class="grid place-content-center text-red rounded-full h-8 w-8 border border-black-accent">
+        <IconPen />
+      </div>
     </div>
   </header>
 </template>
