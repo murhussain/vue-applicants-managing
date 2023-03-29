d1c54553d9a9aa9f37f7ff241d663d7649e933c4
Murhussain <husseinmurashi@yahoo.com>
2023-03-29 10:39:14 +0200

    Done with creating ApplicantsCard

---
+++ src/components/shared/TheApplicants.vue
@@ -0,0 +1,16 @@
+<script setup>
+import ApplicantCard from '@/components/shared/ApplicantCard.vue'
+</script>
+
+<template>
+  <div class="applicant-card-list">
+    <ApplicantCard />
+    <ApplicantCard />
+    <ApplicantCard />
+    <ApplicantCard />
+    <ApplicantCard />
+    <ApplicantCard />
+    <ApplicantCard />
+    <ApplicantCard />
+  </div>
+</template>
--- src/components/shared/TheCategories.vue
+++ src/components/shared/TheCategories.vue
@@ -3,12 +3,14 @@ import CategoryCard from '@/components/shared/CategoryCard.vue'
 import IconStar from '@/components/icons/IconStar.vue'
 import IconCheck from '@/components/icons/IconCheck.vue'
 import IconUserTick from '@/components/icons/IconUserTick.vue'
-import ApplicantCard from '@/components/shared/ApplicantCard.vue'
+import TheApplicants from '@/components/shared/TheApplicants.vue'
 </script>
 
 <template>
   <div class="grid grid-cols-3 gap-6">
+
     <div class="space-y-4">
+      <!-- The category header of single category -->
       <CategoryCard title="New" :completedTasks="3" totalTasks="5" bgColor="bg-[#c8cff7]" 
         levelColor="bg-primary" statColor="text-primary"
       >
@@ -17,14 +19,12 @@ import ApplicantCard from '@/components/shared/ApplicantCard.vue'
         </template>
       </CategoryCard>
       
-      <div class="applicant-card-list space-y-2">
-        <ApplicantCard />
-        <div class="applicant-card"></div>
-        <div class="applicant-card"></div>
-      </div>
+      <!-- The Applicants list -->
+      <TheApplicants />
     </div>
 
     <div class="space-y-4">
+      <!-- The category header of single category -->
       <CategoryCard title="Shortlisted" :completedTasks="5" totalTasks="10" bgColor="bg-secondary-accent" 
         levelColor="bg-secondary" statColor="text-secondary"
       >
@@ -33,12 +33,12 @@ import ApplicantCard from '@/components/shared/ApplicantCard.vue'
         </template>
       </CategoryCard>
 
-      <div class="applicant-card-list">
-        hello here comes list
-      </div>
+      <!-- The Applicants list -->
+      <TheApplicants />
     </div>
 
     <div class="space-y-4">
+      <!-- The category header of single category -->
       <CategoryCard title="Interviewed" :completedTasks="7" totalTasks="10" bgColor="bg-third-accent" 
         levelColor="bg-third" statColor="text-third"
       >
@@ -47,9 +47,8 @@ import ApplicantCard from '@/components/shared/ApplicantCard.vue'
         </template>
       </CategoryCard>
 
-      <div class="applicant-card-list">
-        hello here comes list
-      </div>
+      <!-- The Applicants list -->
+      <TheApplicants />
     </div>
   </div>
 </template>
