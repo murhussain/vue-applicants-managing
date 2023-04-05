55c4ab2a336252d4da2c83605daccb90e9be5b89
Murhussain <husseinmurashi@yahoo.com>
2023-04-05 15:25:11 +0200

    Done with designs for both add&update view

--- src/views/AddJobView.vue
+++ src/views/AddJobView.vue
@@ -1,43 +1,40 @@
 <template>
-  <div className="flex bg-body dark:bg-d-body-accent h-full rounded-lg">
-    <div class="flex-1 grid place-content-center">
-      <div class="w-96 px-4 py-8 bg-[#f1f3fd] dark:bg-d-body-accent-secondary rounded-lg shadow-sm flex flex-col items-center space-y-4">
-        <h3 className='capitalize text-[24px] text-black dark:text-body font-medium'>
-          Create New Job
-        </h3>
-        <form @submit.prevent="onSubmit" class="space-y-4" noValidate>
-          <div class="space-y-2">
-            <label for="name" class="block">
-              <span class="label">Job Name:</span>
-              <input type="text" required id="name" v-model="newJob.name" class="input-type"/>
-            </label>
-            <label for="code" class="block">
-              <span class="label">Job Code:</span>
-              <input type="text" required id="code" v-model="newJob.code" class="input-type"/>
-            </label>
-            <label for="initSalary" class="block">
-              <span class="label">Job Initial Salary:</span>
-              <input type="text" required id="initSalary" v-model="newJob.initSalary" class="input-type"/>
-            </label>
-            <label for="maxSalary" class="block">
-              <span class="label">Job Maximum Salary:</span>
-              <input type="text" required id="maxSalary" v-model="newJob.maxSalary" class="input-type"/>
-            </label>
-          </div>
-          <div>
-            <button
-              className="w-full bg-primary hover:bg-primary-dark transition-all py-2.5 px-10 rounded-md
-              text-white font-bold text-lg text-center"
-              type="submit"
-            >
-              Create
-            </button>
-          </div>
-          
-        </form>
-      </div>
+  <div className="grid place-content-center  h-full">
+    <div class="w-96 px-4 py-8 bg-body dark:bg-d-body-accent-secondary rounded-lg shadow-sm flex flex-col items-center space-y-4">
+      <h3 className='capitalize text-[24px] text-black dark:text-body font-medium'>
+        Create New Job
+      </h3>
+      <form @submit.prevent="onSubmit" class="space-y-4" noValidate>
+        <div class="space-y-2">
+          <label for="name" class="block">
+            <span class="label">Job Name:</span>
+            <input type="text" required id="name" v-model="newJob.name" class="input-type"/>
+          </label>
+          <label for="code" class="block">
+            <span class="label">Job Code:</span>
+            <input type="text" required id="code" v-model="newJob.code" class="input-type"/>
+          </label>
+          <label for="initSalary" class="block">
+            <span class="label">Job Initial Salary:</span>
+            <input type="text" required id="initSalary" v-model="newJob.initSalary" class="input-type"/>
+          </label>
+          <label for="maxSalary" class="block">
+            <span class="label">Job Maximum Salary:</span>
+            <input type="text" required id="maxSalary" v-model="newJob.maxSalary" class="input-type"/>
+          </label>
+        </div>
+        <div>
+          <button
+            className="w-full bg-primary hover:bg-primary-dark transition-all py-2.5 px-10 rounded-md
+            text-white font-bold text-lg text-center"
+            type="submit"
+          >
+            Create
+          </button>
+        </div>
+        
+      </form>
     </div>
-    <div class="w-96 bg-primary dark:bg-primary-dark rounded-r-lg"></div>
   </div>
 </template>
 
--- src/views/EditJobView.vue
+++ src/views/EditJobView.vue
@@ -1,41 +1,39 @@
 <template>
-  <div class="flex bg-body dark:bg-d-body-accent h-full rounded-lg">
-    <div class="flex-1 grid place-content-center">
-      <div class="w-96 px-4 py-8 bg-[#f1f3fd] dark:bg-d-body-accent-secondary rounded-lg shadow-sm flex flex-col items-center space-y-4">
-        <h3 className='capitalize text-[24px] text-black dark:text-body font-medium'>
-          Update The Job
-        </h3>
-        <form @submit.prevent="onSubmit" class="space-y-4">
-          <div class="space-y-2">
-            <label for="name" class="block">
-              <span class="label">Job Name:</span>
-              <input type="text" required id="name" class="input-type" :value="selectedJob.name" ref="nameInputRef" />
-            </label>
-            <label for="code" class="block">
-              <span class="label">Job Code:</span>
-              <input type="text" required id="code" class="input-type" :value="selectedJob.code" ref="codeInputRef" />
-            </label>
-            <label for="initSalary" class="block">
-              <span class="label">Job Initial Salary:</span>
-              <input type="text" required id="initSalary" class="input-type" :value="selectedJob.initSalary" ref="initSalaryInputRef" />
-            </label>
-            <label for="maxSalary" class="block">
-              <span class="label">Job Maximum Salary:</span>
-              <input type="text" required id="maxSalary" class="input-type" :value="selectedJob.maxSalary" ref="maxSalaryInputRef" />
-            </label>
-          </div>
-          <div>
-            <button
-              class="w-full bg-primary hover:bg-primary-dark transition-all py-2.5 px-10 rounded-md text-white font-bold text-lg text-center"
-              type="submit"
-            >
-              Update
-            </button>
-          </div>
-        </form>
-      </div>
+  <div class="grid place-content-center h-full">
+    <div class="w-96 px-4 py-8 bg-body dark:bg-d-body-accent-secondary rounded-lg shadow-sm flex flex-col items-center space-y-4">
+      <h3 className='capitalize text-[24px] text-black dark:text-body font-medium'>
+        Update The Job
+      </h3>
+      <form @submit.prevent="onSubmit" class="space-y-4">
+        <div class="space-y-2">
+          <label for="name" class="block">
+            <span class="label">Job Name:</span>
+            <input type="text" required id="name" class="input-type" :value="selectedJob.name" ref="nameInputRef" />
+          </label>
+          <label for="code" class="block">
+            <span class="label">Job Code:</span>
+            <input type="text" required id="code" class="input-type" :value="selectedJob.code" ref="codeInputRef" />
+          </label>
+          <label for="initSalary" class="block">
+            <span class="label">Job Initial Salary:</span>
+            <input type="text" required id="initSalary" class="input-type" :value="selectedJob.initSalary" ref="initSalaryInputRef" />
+          </label>
+          <label for="maxSalary" class="block">
+            <span class="label">Job Maximum Salary:</span>
+            <input type="text" required id="maxSalary" class="input-type" :value="selectedJob.maxSalary" ref="maxSalaryInputRef" />
+          </label>
+        </div>
+        <div>
+          <button
+            class="w-full bg-primary hover:bg-primary-dark transition-all py-2.5 px-10 rounded-md text-white font-bold text-lg text-center"
+            type="submit"
+          >
+            Update
+          </button>
+        </div>
+      </form>
     </div>
-    <div class="w-96 bg-primary dark:bg-primary-dark rounded-r-lg"></div>
+    
   </div>
 </template>
 
