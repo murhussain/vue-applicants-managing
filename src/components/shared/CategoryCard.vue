89659f5b362555918332a4e5fc5dd9899488d616
Murhussain <husseinmurashi@yahoo.com>
2023-04-25 11:43:32 +0200

    Done with responsive for job applicants

--- src/components/shared/CategoryCard.vue
+++ src/components/shared/CategoryCard.vue
@@ -1,12 +1,12 @@
 <template>
-  <div :class="bgColor" class="relative h-[2.7rem] xl:h-12 rounded-xl flex justify-end items-center">
+  <div :class="bgColor" class="relative h-[2.7rem] sm:h-[2.8rem] lg:h-[3rem] xl:h-12 rounded-xl flex justify-end items-center">
     <div :class="[levelColor, 'progress-bar']" 
-      class="absolute h-[2.7rem] xl:h-12 top-0 left-0 rounded-xl flex items-center"
+      class="absolute h-[2.7rem] sm:h-[2.8rem] lg:h-[3rem] xl:h-12 top-0 left-0 rounded-xl flex items-center"
       :style="{ width: progressBarWidth, background: levelColor }"
     >
       <div class="ml-4 flex items-center space-x-2">
         <slot name="icon" />
-        <p class="text-white xl:text-xl font-medium" v-if="title">{{ title }}</p>
+        <p class="text-white lg:text-xl font-medium" v-if="title">{{ title }}</p>
       </div>
     </div>
     <p :class="statColor" class="text-sm font-medium mr-4 animate-bounce ">
--- src/views/TestingView.vue
+++ src/views/TestingView.vue
@@ -1,24 +1,13 @@
 <template>
-   <div v-if="loading" class="h-full grid place-content-center">
+  <div v-if="loading" class="h-full grid place-content-center">
     <LoaderXl />
   </div>
   <div v-show="!loading" class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-6">
     <div class="applicant-card-list"
       @drop="onDrop('new', $event)" 
-      @dragover.prevent
+      @dragover.prevent="onDragOver('new', $event)"
     >
       <h2 class="text-lg font-bold mb-4">New Applicants</h2>
-      <!-- <ul class="space-y-2">
-        <li 
-          v-for="applicant in getApplicantsByCategory('new')" 
-          :key="applicant.id" 
-          :draggable="true" @dragstart="onDragStart(applicant, $event)" 
-          :data-category="'new'"
-        >
-          <h3 class="font-medium">{{ applicant.name }}</h3>
-          <p class="text-sm">{{ applicant.position }}</p>
-        </li>
-      </ul> -->
       <ApplicantCard
         v-for="applicant in getApplicantsByCategory('new')" 
         :key="applicant.id" 
@@ -28,24 +17,13 @@
         :position="applicant.position"
         :skills="applicant.skills"
       />
+      <div class="placeholder" ref="newPlaceholder" />
     </div>
     <div class="applicant-card-list"
       @drop="onDrop('shortlisted', $event)" 
-      @dragover.prevent
+      @dragover.prevent="onDragOver('shortlisted', $event)"
     >
       <h2 class="text-lg font-bold mb-4">Shortlisted Applicants</h2>
-      <!-- <ul class="space-y-2">
-        <li 
-          v-for="applicant in getApplicantsByCategory('shortlisted')" 
-          :key="applicant.id" 
-          :draggable="true" 
-          @dragstart="onDragStart(applicant, $event)" 
-          :data-category="'shortlisted'"
-        >
-          <h3 class="font-medium">{{ applicant.name }}</h3>
-          <p class="text-sm">{{ applicant.position }}</p>
-        </li>
-      </ul> -->
       <ApplicantCard
         v-for="applicant in getApplicantsByCategory('shortlisted')" 
         :key="applicant.id" 
@@ -55,23 +33,13 @@
         :position="applicant.position"
         :skills="applicant.skills"
       />
+      <div class="placeholder" ref="shortlistedPlaceholder" />
     </div>
     <div class="applicant-card-list"
       @drop="onDrop('interviewed', $event)" 
-      @dragover.prevent
+      @dragover.prevent="onDragOver('interviewed', $event)"
     >
       <h2 class="text-lg font-bold mb-4">Interviewed Applicants</h2>
-      <!-- <ul class="space-y-2">
-        <li 
-          v-for="applicant in getApplicantsByCategory('interviewed')" 
-          :key="applicant.id" 
-          :draggable="true" @dragstart="onDragStart(applicant, $event)" 
-          :data-category="'interviewed'"
-        >
-          <h3 class="font-medium">{{ applicant.name }}</h3>
-          <p class="text-sm">{{ applicant.position }}</p>
-        </li>
-      </ul> -->
       <ApplicantCard
         v-for="applicant in getApplicantsByCategory('interviewed')" 
         :key="applicant.id" 
@@ -81,6 +49,7 @@
         :position="applicant.position"
         :skills="applicant.skills"
       />
+      <div class="placeholder" ref="interviewedPlaceholder" />
     </div>
   </div>
 </template>
@@ -95,6 +64,7 @@ import ApplicantCard from '@/components/shared/ApplicantCard.vue'
 const applicantsStore = useApplicantsStore();
 const { applicants, loading } = storeToRefs(useApplicantsStore());
 
+let draggedApplicant = null;
 
 onMounted(async () => {
   await applicantsStore.fetchAndSetApplicants();
@@ -105,19 +75,38 @@ function getApplicantsByCategory(category) {
 }
 
 function onDragStart(applicant, event) {
-  event.dataTransfer.setData('text/plain', applicant.id)
+  event.dataTransfer.setData('text/plain', applicant.id);
+  draggedApplicant = applicant;
+  event.dataTransfer.setDragImage(0, 0, 0);
+}
+
+function onDragOver(event) {
+  event.preventDefault();
 }
 
 function onDrop(category, event) {
-  const applicantId = event.dataTransfer.getData('text/plain')
-  const applicant = applicants.value.find(a => a.id.toString() === applicantId)
-
+  event.preventDefault();
+  const applicantId = event.dataTransfer.getData('text/plain');
+  const applicant = applicants.value.find(a => a.id.toString() === applicantId);
   if (applicant.category !== category) {
-    applicant.category = category
-    applicantsStore.updateApplicantCategory(applicant.id, category)
+    applicant.category = category;
+    applicantsStore.updateApplicantCategory(applicant.id, category);
   }
-
-  event.target.appendChild(document.getElementById(applicantId))
+  const targetElement = event.target.closest('.applicant-card-list');
+  if (targetElement) {
+    const targetCategory = targetElement.dataset.category;
+    if (draggedApplicant && targetCategory !== draggedApplicant.category) {
+      const newApplicant = {
+        id: Date.now(),
+        name: '',
+        position: '',
+        skills: '',
+        category: targetCategory
+      };
+      applicantsStore.addApplicant(newApplicant);
+      draggedApplicant = newApplicant;
+    }
+    event.target.appendChild(document.getElementById(applicantId));
+  }
 }
-
 </script>
