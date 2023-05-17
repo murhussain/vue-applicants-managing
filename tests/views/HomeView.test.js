0b02698b0b88989683773c4bdf106b89530a3306
Murhussain <husseinmurashi@yahoo.com>
2023-05-17 14:20:25 +0200

    Done with adding tests on HomeView

---
+++ tests/views/HomeView.test.js
@@ -0,0 +1,40 @@
+import { shallowMount, config } from '@vue/test-utils';
+import { useI18n } from "vue-i18n";
+import { createTestingPinia } from "@pinia/testing";
+import { describe, test, it, vi, expect, beforeEach } from "vitest";
+import HomeView from '@/views/HomeView.vue';
+import TheCategories from '@/components/shared/TheCategories.vue';
+
+vi.mock("vue-i18n");
+
+useI18n.mockReturnValue({
+  t: (tKey) => tKey,
+});
+
+config.global.mocks = {
+  $t: (tKey) => tKey,
+};
+
+describe('HomeView Page', () => {
+  let wrapper = null;
+
+  beforeEach(() => {
+    wrapper = shallowMount(HomeView, {
+      global: {
+        plugins: [
+          createTestingPinia({
+            createSpy: vi.fn,
+          }),
+        ],
+      },
+    });  
+  });
+
+  test("renders correctly", () => {
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('should render TheCategories in the template', () => {
+    expect(wrapper.findComponent(TheCategories).exists()).toBe(true);
+  });
+});
